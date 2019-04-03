ea8e23267a8ca3d8d269a27611f31de5cbd9f3b2
Shivam-Bhardwaj <sb6377@nyu.edu>
2019-04-03 04:09:09 -0400

    update

--- src/PRE3.cpp
+++ src/PRE3.cpp
@@ -3,9 +3,26 @@
 //
 
 #include<bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-    cout << "pre3";
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    int d, s = 0;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = n; j > 0; j--) {
+            if (i == j) { d += a[i][j]; }
+        }
+    }
+    cout << d;
     return 0;
 }
