ad2aa018dfea3fef573de45c5f1232fe5f6f6716
shivam-bhardwaj <sb6377@nyu.edu>
2019-04-16 03:54:02 -0400

    PRE4 DONE

--- src/PRE4.cpp
+++ src/PRE4.cpp
@@ -7,6 +7,18 @@
 using namespace std;
 
 int main() {
+    int n;
+    int a[100000];
+    cin >> n;
 
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int j = 0; j < n / 2; j++) {
+        int sum = a[j] + a[n - (j) - 1];
+        cout << sum / 10 << " " << sum % 10 << endl;
+
+    }
     return 0;
 }
